48a181a1b4cf4765a3d90fe7943aec5db7ad8eaf
ntdung100392 <dung.nt100392@gmail.com>
2015-02-14 22:24:51 +0700

    Modify Url

--- admin/config.php
+++ admin/config.php
@@ -4,22 +4,22 @@ define('HTTP_SERVER', 'http://trungdung.ddns.net/YobeShopv20/admin/');
 define('HTTP_CATALOG', 'http://trungdung.ddns.net/YobeShopv20/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8080/YobeShopv20/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8080/YobeShopv20/');
+define('HTTPS_SERVER', 'http://trungdung.ddns.net/YobeShopv20/admin/');
+define('HTTPS_CATALOG', 'http://trungdung.ddns.net/YobeShopv20/');
 
 // DIR
-define('DIR_APPLICATION', '/yobeshopv20/admin/');
-define('DIR_SYSTEM', '/yobeshopv20/system/');
-define('DIR_LANGUAGE', '/yobeshopv20/admin/language/');
-define('DIR_TEMPLATE', '/yobeshopv20/admin/view/template/');
-define('DIR_CONFIG', '/yobeshopv20/system/config/');
-define('DIR_IMAGE', '/yobeshopv20/image/');
-define('DIR_CACHE', '/yobeshopv20/system/cache/');
-define('DIR_DOWNLOAD', 'D:\YobeShopv20/system/download/');
-define('DIR_UPLOAD', '/yobeShopv20/system/upload/');
-define('DIR_LOGS', '/yobeShopv20/system/logs/');
-define('DIR_MODIFICATION', '/yobeshopv20/system/modification/');
-define('DIR_CATALOG', '/yobeshopv20/catalog/');
+define('DIR_APPLICATION', '/publish/yobeshopv20/admin/');
+define('DIR_SYSTEM', '/publish/yobeshopv20/system/');
+define('DIR_LANGUAGE', '/publish/yobeshopv20/admin/language/');
+define('DIR_TEMPLATE', '/publish/yobeshopv20/admin/view/template/');
+define('DIR_CONFIG', '/publish/yobeshopv20/system/config/');
+define('DIR_IMAGE', '/publish/yobeshopv20/image/');
+define('DIR_CACHE', '/publish/yobeshopv20/system/cache/');
+define('DIR_DOWNLOAD', '/publish/yobeshopv20/system/download/');
+define('DIR_UPLOAD', '/publish/yobeShopv20/system/upload/');
+define('DIR_LOGS', '/publish/yobeShopv20/system/logs/');
+define('DIR_MODIFICATION', '/publish/yobeshopv20/system/modification/');
+define('DIR_CATALOG', '/publish/yobeshopv20/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://trungdung.ddns.net/yobeshopv20/');
 define('HTTPS_SERVER', 'http://trungdung.ddns.net/yobeshopv20/');
 
 // DIR
-define('DIR_APPLICATION', '/yobeshopv20/catalog/');
-define('DIR_SYSTEM', '/yobeshopv20/system/');
-define('DIR_LANGUAGE', '/yobeshopv20/catalog/language/');
-define('DIR_TEMPLATE', '/yobeshopv20/catalog/view/theme/');
-define('DIR_CONFIG', '/yobeshopv20/system/config/');
-define('DIR_IMAGE', '/yobeshopv20/image/');
-define('DIR_CACHE', '/yobeshopv20/system/cache/');
-define('DIR_DOWNLOAD', '/yobeshopv20/system/download/');
-define('DIR_UPLOAD', '/yobeshopv20/system/upload/');
-define('DIR_MODIFICATION', '/yobeshopv20/system/modification/');
-define('DIR_LOGS', '/yobeshopv20/system/logs/');
+define('DIR_APPLICATION', '/publish/yobeshopv20/catalog/');
+define('DIR_SYSTEM', '/publish/yobeshopv20/system/');
+define('DIR_LANGUAGE', '/publish/yobeshopv20/catalog/language/');
+define('DIR_TEMPLATE', '/publish/yobeshopv20/catalog/view/theme/');
+define('DIR_CONFIG', '/publish/yobeshopv20/system/config/');
+define('DIR_IMAGE', '/publish/yobeshopv20/image/');
+define('DIR_CACHE', '/publish/yobeshopv20/system/cache/');
+define('DIR_DOWNLOAD', '/publish/yobeshopv20/system/download/');
+define('DIR_UPLOAD', '/publish/yobeshopv20/system/upload/');
+define('DIR_MODIFICATION', '/publish/yobeshopv20/system/modification/');
+define('DIR_LOGS', '/publish/yobeshopv20/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
